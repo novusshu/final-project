25c38febcc306cb85decda9a96ca1cd0ee3e45b5
novusshu <novusshu@gmail.com>
2021-12-20 14:00:14 -0600

    all

---
+++ bot/app.py
@@ -0,0 +1,76 @@
+# referrence: https://www.toptal.com/python/telegram-bot-tutorial-python
+from huggingface_hub.inference_api import InferenceApi
+from flask import Flask, request
+import telegram
+from telebot.credentials import bot_token, bot_user_name, URL, api_token
+
+
+global bot
+global TOKEN
+global API_TOKEN
+TOKEN = bot_token
+API_TOKEN = api_token
+bot = telegram.Bot(token=TOKEN)
+
+inference = InferenceApi(
+    repo_id="novusshu/DialoGPT-medium-crypto",
+    # repo_id="microsoft/DialoGPT-large",
+    token=API_TOKEN)
+
+app = Flask(__name__)
+
+
+@app.route('/{}'.format(TOKEN), methods=['POST'])
+def respond():
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
+
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    text = update.message.text.encode('utf-8').decode()
+    # for debugging purposes only
+    print("got text message :", text)
+    # the first time you chat with the bot AKA the welcoming message
+    if text == "/start":
+        # print the welcoming message
+        bot_welcome = """
+        Hi, I know things about cryptocurrency. Now talk to me!
+       """
+        # send the welcoming message
+        bot.sendMessage(chat_id=chat_id, text=bot_welcome,
+                        reply_to_message_id=msg_id)
+
+    else:
+        try:
+            # clear the message we got from any non alphabets
+            res = inference(text)
+
+            bot.sendMessage(chat_id=chat_id, text=res['generated_text'],
+                            reply_to_message_id=msg_id)
+
+        except Exception:
+            # if things went wrong
+            bot.sendMessage(
+                chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
+
+    return 'ok'
+
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+
+@app.route('/')
+def index():
+    return '.'
+
+
+if __name__ == '__main__':
+    app.run(threaded=True)
